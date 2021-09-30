3df03d2ccc460b8bdbfb491bd0c76978f61dc81c
Rao Zvorovski <demon@L.localdomain>
2021-10-01 01:13:04 +0300

    add freebasic support

--- cms/grading/languages/freebasicqb.py
+++ cms/grading/languages/freebasicqb.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Contest Management System - http://cms-dev.github.io/
-# Copyright © 2021 Ahto Truu
+# Copyright © 2021 Rao Zvorovski
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -28,32 +27,33 @@ from future.builtins import *  # noqa
 from cms.grading import CompiledLanguage
 
 
-__all__ = ["GoLang"]
+__all__ = ["FreeBasic"]
 
 
-class GoLang(CompiledLanguage):
-    """This defines the GO programming language.
+class FreeBasicQb(CompiledLanguage):
+    """
+    This defines the GO programming language.
     """
 
     @property
     def name(self):
         """See Language.name."""
-        return "GoLang"
+        return "FreeBasic (QB)"
 
     @property
     def source_extensions(self):
         """See Language.source_extensions."""
-        return [".go"]
+        return [".bas"]
 
     @property
     def requires_multithreading(self):
         """See Language.requires_multithreading."""
-        return True
+        return False
 
     def get_compilation_commands(self,
                                  source_filenames, executable_filename,
                                  for_evaluation=True):
         """See Language.get_compilation_commands."""
-        compile_command = ["/usr/bin/go", "build",
-                           "-o", executable_filename]
+        compile_command = ["/usr/bin/fbc", "-lang", "qb",
+                           "-x", executable_filename, "-b", *source_filenames]
         return [compile_command]
