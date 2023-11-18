2e6c17ebe00b340bdcf1efa43ed163d3d44ed64e
prandla <peeter.a.randlaa@gmail.com>
2023-11-18 05:45:47 +0200

    add Go
    todo for upstreaming: add tests and update installation docs

--- setup.py
+++ setup.py
@@ -194,6 +194,7 @@ setup(
             "Python 2 / CPython=cms.grading.languages.python2_cpython:Python2CPython",
             "Python 3 / CPython=cms.grading.languages.python3_cpython:Python3CPython",
             "Rust=cms.grading.languages.rust:Rust",
+            "Go=cms.grading.languages.golang:GoLang",
         ],
     },
     keywords="ioi programming contest grader management system",
